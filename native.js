ebfbd962a58aabccbe6a9a1ba94c2652e2214d18
Niass <cheroubawan@hotmail.fr>
2022-07-04 11:07:02 +0200

    testing

---
+++ native.js
@@ -0,0 +1,66 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+  eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"adDecodeData\": () => (/* binding */ adDecodeData)\n/* harmony export */ });\nvar adDecodeData = function (encodedText) {\n    return JSON.parse(atob(encodedText));\n};\nvar infoFromWindowName = function () {\n    try {\n        var res = adDecodeData(window.name);\n        console.log('res', res);\n        return adDecodeData(window.name);\n    }\n    catch (_a) {\n        return {};\n    }\n};\ninfoFromWindowName();\n\n\n//# sourceURL=webpack://cookies-sync/./src/index.ts?");
+
+  /***/ })
+  
+  /******/ 	});
+  /************************************************************************/
+  /******/ 	// The require scope
+  /******/ 	var __webpack_require__ = {};
+  /******/ 	
+  /************************************************************************/
+  /******/ 	/* webpack/runtime/define property getters */
+  /******/ 	(() => {
+  /******/ 		// define getter functions for harmony exports
+  /******/ 		__webpack_require__.d = (exports, definition) => {
+  /******/ 			for(var key in definition) {
+  /******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+  /******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+  /******/ 				}
+  /******/ 			}
+  /******/ 		};
+  /******/ 	})();
+  /******/ 	
+  /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+  /******/ 	(() => {
+  /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+  /******/ 	})();
+  /******/ 	
+  /******/ 	/* webpack/runtime/make namespace object */
+  /******/ 	(() => {
+  /******/ 		// define __esModule on exports
+  /******/ 		__webpack_require__.r = (exports) => {
+  /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+  /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+  /******/ 			}
+  /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+  /******/ 		};
+  /******/ 	})();
+  /******/ 	
+  /************************************************************************/
+  /******/ 	
+  /******/ 	// startup
+  /******/ 	// Load entry module and return exports
+  /******/ 	// This entry module can't be inlined because the eval devtool is used.
+  /******/ 	var __webpack_exports__ = {};
+  /******/ 	__webpack_modules__["./src/index.ts"](0, __webpack_exports__, __webpack_require__);
+  /******/ 	
+  /******/ })()
+  ;
